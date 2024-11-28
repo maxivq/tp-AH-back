b9665e2dd7a1aaa58c8eabc32ced337171f9a986
maxivq <kitvelazquez@gmail.com>
2024-11-28 15:28:58 -0300

    Initial commit

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import User from '../../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'El correo electrónico ya está registrado' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id, role: newUser.role }, 'secret', { expiresIn: '1h' });
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al registrar el usuario' });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Correo electrónico o contraseña incorrectos' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Correo electrónico o contraseña incorrectos' });
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, 'secret', { expiresIn: '1h' });
+
+    res.status(200).json({ token, role: user.role });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al iniciar sesión' });
+  }
+};
